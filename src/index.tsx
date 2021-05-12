643cbfc7a14ad421c816a976492a9aa6febd9767
Eli Storlie <20595774+eli215@users.noreply.github.com>
2021-05-11 22:05:37 -0700

    create new React app from typescript template; add code from VX Link Types demo (vxdemo-tree) to use for our tree view

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // or import { render } from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import ParentSize from '@vx/responsive/lib/components/ParentSize';
+import Example from './Example';
+import './sandbox-styles.css';
+
+ReactDOM.render(
+  // <App />,
+  <React.StrictMode>  {/* StrictMode = a helper component; gives warnings that aid in development. */} 
+    <ParentSize>{({ width, height }) => 
+      <Example width={width} height={height} />}
+    </ParentSize>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
