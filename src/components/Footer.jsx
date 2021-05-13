38647f22f3db47b65a57d381b80fbe6f2cee6db1
Eli Storlie <20595774+eli215@users.noreply.github.com>
2021-05-12 19:24:56 -0700

    add album cover, song, artist to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,24 @@
-
 import React from "react";
+import "./Footer.css"
 
 function Footer() {
   return (
     <div className="footer">
-      <footer class="py-5 bg-dark fixed-bottom">
+      <footer class="py-2 bg-dark fixed-bottom">
         <div class="container">
-          <p class="m-0 text-center text-white">
-            Copyright &copy; Music Discovery App 2021
-          </p>
+          <div class="section">
+              <div class="section img">
+                <img src="Imagination_FtP.jpg" width="115" height="115"></img>
+              </div>
+              <div class="section content">
+                <div class="section title">
+                  Imagination
+                </div>
+                <div class="section descr">
+                  Foster the People
+                </div>
+              </div>
+          </div>
         </div>
       </footer>
     </div>
