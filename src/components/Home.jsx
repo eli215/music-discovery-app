c4b461813827d01d91c091ccdb3a9f1153269b40
Eli Storlie <20595774+eli215@users.noreply.github.com>
2021-06-01 14:59:20 -0700

    update searchbar style & route searches to List View

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,52 +1,30 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import { Link } from "react-router-dom";
+import { Input, Button, Icon } from 'semantic-ui-react'
 import "./Home.css"
+
 
 function Home() {
   render();
   return (
-    <div class="flex-center">
+    // <div class="flex-center">
       <Form />
-    </div>
+    // </div>
   );
-  // return (
-  //   <div className="home">
-  //     <div class="container">
-  //       <div class="row align-items-center my-5">
-  //         <div class="col-lg-7">
-  //           <img
-  //             class="img-fluid rounded mb-4 mb-lg-0"
-  //             src="http://placehold.it/900x400"
-  //             alt=""
-  //           />
-  //         </div>
-  //         <div class="col-lg-5">
-  //           <h1 class="font-weight-light">Home</h1>
-  //           <p>
-  //             Lorem Ipsum is simply dummy text of the printing and typesetting
-  //             industry. Lorem Ipsum has been the industry's standard dummy text
-  //             ever since the 1500s, when an unknown printer took a galley of
-  //             type and scrambled it to make a type specimen book.
-  //           </p>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 }
 
 class Form extends React.Component {
 	render() {
   	return (
-    	<form>
-      <span className="formtext"></span>
-    	  <input 
-          type="text" 
-          placeholder="Enter a song/artist..." 
-          required 
-        />
-        <button>Go!</button>
-    	</form>
+      <div class="center">
+        <Input size='big' action={
+          <Button icon size='big' as={Link} to="/listview">
+            <Icon name='search'/>
+          </Button>
+        }
+       placeholder='Enter a song/artist...' />
+    	</div>
     );
   }
 }
