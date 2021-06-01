46e8d246049c13c922eca45e50cc33a4cc322c54
Thomas Galios <77800766+ThomasEmilio@users.noreply.github.com>
2021-06-01 07:01:35 -1000

    Update Listview.jsx
    We can see the tags now and it looks good not just an ugly alert

--- src/components/Listview.jsx
+++ src/components/Listview.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { FixedSizeList } from 'react-window';
 import songSelection from './database';
-import { colors, Divider, hexToRgb, List, ListItemAvatar, ListItemIcon, Typography } from '@material-ui/core';
+import { colors, Divider, hexToRgb, List, ListItemAvatar, ListItemIcon, Tooltip, Typography } from '@material-ui/core';
 import DiscIcon from '@material-ui/icons/PlayCircleFilled';
 import { createMuiTheme } from '@material-ui/core/styles';
 import SpotifyImage from './Images/defaultSpotify.png';
 import albumCover from './Images/lostInYourLight.jpg';
-import Modal from '@material-ui/core/Modal';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,11 +38,26 @@ const myTheme = createMuiTheme({
   },
 });
 
+const HtmlTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: '#f5f5f9',
+    color: 'rgba(0, 0, 0, 0.87)',
+    maxWidth: 900,
+    fontSize: theme.typography.pxToRem(16),
+    border: '1px solid #dadde9',
+  },
+}))(Tooltip);
+
 function renderRow(props) {
   const { index, style } = props;
 
   return (
-      <ListItem button style={style} key={index} divider={true} onClick={() => { alert("Tags: " + songSelection()[index].tags)}}>
+    <HtmlTooltip placement='bottom-start' title={
+      <React.Fragment>
+        <Typography color="inherit" style={{fontSize: 18}}><b>Tags</b></Typography>
+          {songSelection()[index].tags}
+      </React.Fragment>}>
+      <ListItem button style={style} key={index} divider={true}>
 
         <ListItemIcon>
           <DiscIcon style={{ color: '#26deb0' }} />
@@ -53,8 +67,12 @@ function renderRow(props) {
           primary={songSelection()[index].name}
           secondary={<Typography color='#00aaff'> {songSelection()[index].artist} </Typography>} />
       </ListItem>
+    </HtmlTooltip>
 
-    
+
+
+
+
   );
 }
 
@@ -67,12 +85,12 @@ export default function VirtualizedList() {
   const classes = useStyles();
 
   return (
-    <div class = 'row'>
-      <div className={classes.custCol} style={{padding:50}}>
-        <img src={albumCover} alt="Song" width="500" height="500"/>
-        <h1 style={{color: '#26deb0', padding: 10}}>{songSelection()[14].name}</h1>
-        <p style={{color: 'white' }}>{songSelection()[14].artist}</p>
-        <p style={{color: 'white' }}>Tags: {songSelection()[14].tags}</p>
+    <div class='row'>
+      <div className={classes.custCol} style={{ padding: 50 }}>
+        <img src={albumCover} alt="Song" width="500" height="500" />
+        <h1 style={{ color: '#26deb0', padding: 10 }}>{songSelection()[14].name}</h1>
+        <p style={{ color: 'white' }}>{songSelection()[14].artist}</p>
+        <p style={{ color: 'white' }}>Tags: {songSelection()[14].tags}</p>
 
       </div>
       <div className={classes.custCol}>
@@ -80,7 +98,7 @@ export default function VirtualizedList() {
           {renderRow}
         </FixedSizeList>
       </div>
-      
+
     </div>
 
   );
