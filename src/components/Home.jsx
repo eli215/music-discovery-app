d575165aac6a9c58bf8e13168a36163abb0540b4
Eli Storlie <20595774+eli215@users.noreply.github.com>
2021-06-01 17:33:55 -0700

    update homepage style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,29 +2,49 @@ import { render } from "@testing-library/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Input, Button, Icon } from 'semantic-ui-react'
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import "./Home.css"
 
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    height: 400,
+    maxWidth: 300,
+    backgroundColor: theme.palette.background.paper,
+  },
+
+  custCol: {
+    width: '50%',
+    height: '100',
+    backgroundColor: 'black',//'#272b4d',
+  },
+
+}));
+
 function Home() {
+  const classes = useStyles();
+  
   render();
   return (
-    // <div class="flex-center">
+    // ?? style necessary for bg color
+    <div>
       <Form />
-    // </div>
+    </div>
   );
 }
 
 class Form extends React.Component {
 	render() {
   	return (
-      <div class="center">
-        <Input size='big' action={
-          <Button icon size='big' as={Link} to="/listview">
-            <Icon name='search'/>
-          </Button>
-        }
-       placeholder='Enter a song/artist...' />
-    	</div>
+      <div className="center">
+      <Input size='huge' action={
+        <Button icon size='huge' as={Link} to="/listview">
+          <Icon name='search'/>
+        </Button>
+      }
+      placeholder='Enter a song/artist...' />
+      </div>
     );
   }
 }
